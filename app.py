7ff9a81c2905f38dccb8beb56375d0948395c160
Kestutis Januskevicius <infohata@gmail.com>
2023-05-26 10:56:15 +0200

    vardenis pavardenis page

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/vardenis_pavardenis/')
+def vardenis_pavardenis():
+    return render_template('vardenis_pavardenis.html/')
+
 if __name__ == "__main__":
     app.run(debug=True)
