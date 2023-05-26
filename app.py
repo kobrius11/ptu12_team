89b42f335b12f96fc20569cd8877152546698f34
Honichiwa <tadas.zupka@gmail.com>
2023-05-26 13:43:58 +0300

    Tado page

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/vardenis_pavardenis/')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/vardenis_pavardenis/")
 def vardenis_pavardenis():
-    return render_template('vardenis_pavardenis.html/')
+    return render_template("vardenis_pavardenis.html/")
+
+
+@app.route("/tadas_z/")
+def tadas_z():
+    return render_template("/tadas_z.html/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
