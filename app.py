8b8ce1346d287ff0f262661edeb1be205ac83342
Petras Anskaitis <sartep1996@gmail.com>
2023-05-26 14:18:31 +0300

    template done

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/petras_ans/')
+def petras_anskaitis():
+    return render_template('petras_ans.html/')
+
 if __name__ == "__main__":
     app.run(debug=True)
