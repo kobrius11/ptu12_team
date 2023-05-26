0bf7d547314c103c8e8e3e752c3de181a1c3a36d
RenaldasZ <renald.zvega@gmail.com>
2023-05-26 13:13:16 +0300

    added Renaldas content

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/renaldas_zvega')
+def renaldas():
+    return render_template('renaldas_zvega.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
