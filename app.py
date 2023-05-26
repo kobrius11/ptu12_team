5bd1d803a6130c6704de41444ead227818b70933
EricWebDev <erikasj1999@gmail.com>
2023-05-26 14:17:40 +0300

    Erikas Jankauskas web

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/vardenis_pavardenis/')
+@app.route('/Erikas_Jankauskas/')
 def vardenis_pavardenis():
-    return render_template('vardenis_pavardenis.html/')
+    return render_template('Erikas_Jankauskas.html/')
 
 if __name__ == "__main__":
     app.run(debug=True)
