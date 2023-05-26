733014174bbeb1ef8e42c60df6814821cf0dafe5
bro256 <bronius256@gmail.com>
2023-05-26 13:54:46 +0300

    Bronius

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ def index():
 
 @app.route('/vardenis_pavardenis/')
 def vardenis_pavardenis():
-    return render_template('vardenis_pavardenis.html/')
+    return render_template('vardenis_pavardenis.html')
+
+@app.route('/bronius_grigaras/')
+def bronius_grigaras():
+    return render_template('bronius_grigaras.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
